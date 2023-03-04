b9266db64352eef8eb697180f82fe4b4e811ce03
shubhamSS9 <shubhamshinde0999@gmail.com>
2023-03-04 23:08:10 +0530

    added  UC2 Check equality of lines

--- src/com/bridglabz/LineComparison.java
+++ src/com/bridglabz/LineComparison.java
@@ -4,19 +4,40 @@ import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
+        double x1,x2,y1,y2;
+        double x3,x4,y3,y4;
+        double LengthofLine1,LengthofLine2;
+        Scanner inputvalues = new Scanner(System.in);
+        //lengthofline1 calculation
+        System.out.println("Enter the value of x1 :");
+        x1 = inputvalues.nextInt();
+        System.out.println("Enter the value of y1 :");
+        y1 = inputvalues.nextInt();
+        System.out.println("Enter the value of x2 :");
+        x2 = inputvalues.nextInt();
+        System.out.println("Enter the value of y2 :");
+        y2 = inputvalues.nextInt();
+        LengthofLine1 = Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+        System.out.println("The length of a line =" +LengthofLine1);
 
-        System.out.println("Welcome to Line Comparison Computation Program");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter point x1: ");
-        int x1 = sc.nextInt();
-        System.out.println("Enter point y1: ");
-        int y1 = sc.nextInt();
-        System.out.println("Enter point x2: ");
-        int x2 = sc.nextInt();
-        System.out.println("Enter point y2: ");
-        int y2 = sc.nextInt();
-        double length = Math.sqrt((Math.pow((x2 - x1), 2)) + (Math.pow((y2 - y1), 2)));
-        System.out.println("Length of Line between two points: " + length);
+        //lengthofline2 calculation
+        System.out.println("Enter the value of x3 :");
+        x3 = inputvalues.nextInt();
+        System.out.println("Enter the value of y3 :");
+        y3 = inputvalues.nextInt();
+        System.out.println("Enter the value of x4 :");
+        x4 = inputvalues.nextInt();
+        System.out.println("Enter the value of y4 :");
+        y4 = inputvalues.nextInt();
+        LengthofLine2 = Math.sqrt(Math.pow(x4-x3,2)+ Math.pow(y4-y3,2));
+        System.out.println("The length of a line =" +LengthofLine2);
+
+        String Line1=String.valueOf(LengthofLine1);
+        String Line2=String.valueOf(LengthofLine2);
+        if(Line1.equals(Line2))
+            System.out.println("Length of both lines are equal");
+
+        else
+            System.out.println("lengths are not equal");
     }
-    
 }
